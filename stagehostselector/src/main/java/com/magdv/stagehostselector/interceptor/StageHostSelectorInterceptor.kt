bfe9dc54cb04c8c723b60e04fd0c1186f4aba979
n.gurov <n.gurov@magdv.com>
2020-01-15 11:14:02 +0700

    Add possibility work with different path segments

--- stagehostselector/src/main/java/com/magdv/stagehostselector/interceptor/StageHostSelectorInterceptor.kt
+++ stagehostselector/src/main/java/com/magdv/stagehostselector/interceptor/StageHostSelectorInterceptor.kt
@@ -7,7 +7,10 @@ import okhttp3.HttpUrl
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class StageHostSelectorInterceptor(context: Context) : Interceptor {
+class StageHostSelectorInterceptor(
+    context: Context,
+    private val defaultHostUrl: String
+) : Interceptor {
 
     private val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
     private var cachedHostUrl: String? = null
@@ -15,18 +18,26 @@ class StageHostSelectorInterceptor(context: Context) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val hostUrl = sharedPreferences.getString(Constants.HOST_URL_STORAGE_KEY, null)
-            ?: return chain.proceed(chain.request())
+                      ?: return chain.proceed(chain.request())
 
         if (cachedHostUrl != hostUrl) {
             cachedHostUrl = hostUrl
             cachedHostHttpUrl = HttpUrl.parse(hostUrl) ?: throw Exception("Invalid url: $hostUrl")
         }
 
-        val newRequest = cachedHostHttpUrl?.let {
-            val newUrl = chain.request().url().newBuilder()
-                .scheme(it.scheme())
-                .host(it.url().toURI().host)
-                .port(it.port())
+        val newRequest = cachedHostHttpUrl?.let { cachedHostHttpUrl ->
+
+            val defaultHostUrlSegmentsCount = HttpUrl.parse(defaultHostUrl)?.pathSegments()?.count() ?: 0
+            val necessaryPartHostUrlSegmentsCount = if (defaultHostUrlSegmentsCount == 0) 0 else defaultHostUrlSegmentsCount - 1
+            val chainRequestUrlSegmentCount = chain.request().url().pathSegments().count()
+
+            var pathSegments = ""
+            for (i in necessaryPartHostUrlSegmentsCount until chainRequestUrlSegmentCount) {
+                pathSegments += "${chain.request().url().pathSegments()[i]}/"
+            }
+
+            val newUrl = cachedHostHttpUrl.newBuilder()
+                .addPathSegments(pathSegments)
                 .build()
 
             return@let chain.request().newBuilder()
