1a8a300227ed4582e3d52cb18afb4e3a45128c87
Grue1ing <gurovnick@icloud.com>
2020-01-15 14:24:09 +0700

    Add possibility work with different path segments (#4)

--- stagehostselector/src/main/java/com/magdv/stagehostselector/interceptor/StageHostSelectorInterceptor.kt
+++ stagehostselector/src/main/java/com/magdv/stagehostselector/interceptor/StageHostSelectorInterceptor.kt
@@ -7,11 +7,20 @@ import okhttp3.HttpUrl
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class StageHostSelectorInterceptor(context: Context) : Interceptor {
+class StageHostSelectorInterceptor(
+    context: Context,
+    private val defaultHostUrl: String? = null
+) : Interceptor {
 
     private val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
     private var cachedHostUrl: String? = null
     private var cachedHostHttpUrl: HttpUrl? = null
+    private val defaultHostUrlSegments: List<String> by lazy {
+        defaultHostUrl?.let { HttpUrl.parse(it) }
+            ?.pathSegments()
+            ?.filter { it.isNotEmpty() }
+            ?: listOf()
+    }
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val hostUrl = sharedPreferences.getString(Constants.HOST_URL_STORAGE_KEY, null)
@@ -22,12 +31,19 @@ class StageHostSelectorInterceptor(context: Context) : Interceptor {
             cachedHostHttpUrl = HttpUrl.parse(hostUrl) ?: throw Exception("Invalid url: $hostUrl")
         }
 
-        val newRequest = cachedHostHttpUrl?.let {
-            val newUrl = chain.request().url().newBuilder()
-                .scheme(it.scheme())
-                .host(it.url().toURI().host)
-                .port(it.port())
-                .build()
+        val newRequest = cachedHostHttpUrl?.let { cachedHostHttpUrl ->
+            val chainRequestPathSegments = chain.request().url().pathSegments()
+
+            val newUrl = if (shouldReplacePathSegments(defaultHostUrlSegments, chainRequestPathSegments)) {
+                cachedHostHttpUrl.addPathSegments(
+                    chainRequestPathSegments.subList(
+                        fromIndex = defaultHostUrlSegments.count(),
+                        toIndex = chainRequestPathSegments.count()
+                    )
+                )
+            } else {
+                chain.request().url().replaceBaseUrl(cachedHostHttpUrl)
+            }
 
             return@let chain.request().newBuilder()
                 .url(newUrl)
@@ -35,5 +51,32 @@ class StageHostSelectorInterceptor(context: Context) : Interceptor {
         }
 
         return chain.proceed(newRequest ?: chain.request())
+    }
+
+    private fun shouldReplacePathSegments(
+        initialPathSegments: List<String>,
+        requestPathSegments: List<String>
+    ): Boolean {
+        initialPathSegments.forEachIndexed { index, segment ->
+            if (requestPathSegments[index] != segment) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private fun HttpUrl.addPathSegments(pathSegments: List<String>): HttpUrl {
+        return newBuilder()
+            .addPathSegments(pathSegments.joinToString("/") { it })
+            .build()
+    }
+
+    private fun HttpUrl.replaceBaseUrl(newBaseUrl: HttpUrl): HttpUrl {
+        return newBuilder()
+            .scheme(newBaseUrl.scheme())
+            .host(newBaseUrl.host())
+            .port(newBaseUrl.port())
+            .build()
     }
 }
